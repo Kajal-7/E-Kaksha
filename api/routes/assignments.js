af9075d091e2e1766ae434b3fd3b565b120ebddf
Kajal <kajalkaushal700@gmail.com>
2022-04-05 19:54:28 +0530

    Added api folder, socket folder

---
+++ api/routes/assignments.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const Assignment = require("../models/Assig");
+
+
+//get all assignments
+router.get("/",async(req,res)=>{
+    try{
+        const assignments= await Assignment.find();
+        res.status(200).json(assignments);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
+
+//get assignmnet with specific course id
+router.get("/courseid/:cid",async(req,res)=>{
+    try {
+        var query = { courseId: (req.params.cid) };
+        const assignment = await Assignment.find(query);
+        res.status(200).json(assignment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//get an assignment using its _id
+router.get("/:id", async (req, res) => {
+    try {
+        const assignment = await Assignment.findById(req.params.id);
+        res.status(200).json(assignment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//updating submissions in specific assignment
+router.put("/submission/:id", async (req, res) => {    
+    try {
+        
+        const updated =   await Assignment.findByIdAndUpdate(
+            req.params.id, 
+            { $push: { submissions: req.body } },
+            {new : true},
+        );
+        res.status(200).json(updated);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//posting new assignment in db
+router.post("/", async (req, res) => {
+    try {
+        const newAssign = new Assignment(req.body);
+        const response = await newAssign.save();
+
+        res.status(200).json(response);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
